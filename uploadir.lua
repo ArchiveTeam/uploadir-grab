03fc708dd40fe12fd714e6830eb92cfa9bfd27d0
arkiver <arkiver@protonmail.com>
2022-12-18 23:14:07 +0100

    Version 20221218.01. Initial.

---
+++ uploadir.lua
@@ -0,0 +1,285 @@
+dofile("table_show.lua")
+dofile("urlcode.lua")
+local urlparse = require("socket.url")
+local http = require("socket.http")
+local https = require("ssl.https")
+JSON = (loadfile "JSON.lua")()
+
+local item_dir = os.getenv("item_dir")
+local warc_file_base = os.getenv("warc_file_base")
+local item_type = nil
+local item_name = nil
+local item_value = nil
+
+if urlparse == nil or http == nil then
+  io.stdout:write("socket not corrently installed.\n")
+  io.stdout:flush()
+  abortgrab = true
+end
+
+local url_count = 0
+local tries = 0
+local downloaded = {}
+local addedtolist = {}
+local abortgrab = false
+local killgrab = false
+
+local discovered_outlinks = {}
+local discovered_items = {}
+local bad_items = {}
+local ids = {}
+
+local main_download = false
+
+local retry_url = false
+
+for ignore in io.open("ignore-list", "r"):lines() do
+  downloaded[ignore] = true
+end
+
+abort_item = function(item)
+  abortgrab = true
+  if not item then
+    item = item_name
+  end
+  if not bad_items[item] then
+    io.stdout:write("Aborting item " .. item .. ".\n")
+    io.stdout:flush()
+    bad_items[item] = true
+  end
+end
+
+kill_grab = function(item)
+  io.stdout:write("Aborting crawling.\n")
+  killgrab = true
+end
+
+read_file = function(file)
+  if file then
+    local f = assert(io.open(file))
+    local data = f:read("*all")
+    f:close()
+    return data
+  else
+    return ""
+  end
+end
+
+processed = function(url)
+  if downloaded[url] or addedtolist[url] then
+    return true
+  end
+  return false
+end
+
+find_item = function(url)
+  value = string.match(url, "^https?://uploadir%.com/u/([0-9a-zA-Z]+)$")
+  type_ = "u"
+  if value then
+    item_type = type_
+    item_value = value
+    item_name_new = item_type .. ":" .. item_value
+    if item_name_new ~= item_name then
+      ids = {}
+      ids[value] = true
+      abortgrab = false
+      tries = 0
+      item_name = item_name_new
+      print("Archiving item " .. item_name)
+    end
+  end
+end
+
+allowed = function(url, parenturl)
+  if ids[url] then
+    return true
+  end
+
+  if string.match(url, "/reports%?upload_id=") then
+    return false
+  end
+
+  for s in string.gmatch(url, "([0-9a-zA-Z]+)") do
+    if ids[s] then
+      return true
+    end 
+  end
+
+  return false
+end
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  local url = urlpos["url"]["url"]
+  local html = urlpos["link_expect_html"]
+
+  return false
+end
+
+wget.callbacks.get_urls = function(file, url, is_css, iri)
+  local urls = {}
+  local html = nil
+  
+  downloaded[url] = true
+
+  if abortgrab then
+    return {}
+  end
+
+  local function decode_codepoint(newurl)
+    newurl = string.gsub(
+      newurl, "\\[uU]([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F])",
+      function (s)
+        return unicode_codepoint_as_utf8(tonumber(s, 16))
+      end
+    )
+    return newurl
+  end
+
+  local function check(newurl)
+    newurl = decode_codepoint(newurl)
+    local origurl = url
+    local url = string.match(newurl, "^([^#]+)")
+    local url_ = string.match(url, "^(.-)[%.\\]*$")
+    while string.find(url_, "&amp;") do
+      url_ = string.gsub(url_, "&amp;", "&")
+    end
+    if not processed(url_)
+      and allowed(url_, origurl) then
+      table.insert(urls, { url=url_ })
+      addedtolist[url_] = true
+      addedtolist[url] = true
+    end
+  end
+
+  local function checknewurl(newurl)
+    newurl = decode_codepoint(newurl)
+    if string.match(newurl, "['\"><]") then
+      return nil
+    end
+    if string.match(newurl, "^https?:////") then
+      check(string.gsub(newurl, ":////", "://"))
+    elseif string.match(newurl, "^https?://") then
+      check(newurl)
+    elseif string.match(newurl, "^https?:\\/\\?/") then
+      check(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^\\/\\/") then
+      checknewurl(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^//") then
+      check(urlparse.absolute(url, newurl))
+    elseif string.match(newurl, "^\\/") then
+      checknewurl(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^/") then
+      check(urlparse.absolute(url, newurl))
+    elseif string.match(newurl, "^%.%./") then
+      if string.match(url, "^https?://[^/]+/[^/]+/") then
+        check(urlparse.absolute(url, newurl))
+      else
+        checknewurl(string.match(newurl, "^%.%.(/.+)$"))
+      end
+    elseif string.match(newurl, "^%./") then
+      check(urlparse.absolute(url, newurl))
+    end
+  end
+
+  local function checknewshorturl(newurl)
+    newurl = decode_codepoint(newurl)
+    if string.match(newurl, "^%?") then
+      check(urlparse.absolute(url, newurl))
+    elseif not (
+      string.match(newurl, "^https?:\\?/\\?//?/?")
+      or string.match(newurl, "^[/\\]")
+      or string.match(newurl, "^%./")
+      or string.match(newurl, "^[jJ]ava[sS]cript:")
+      or string.match(newurl, "^[mM]ail[tT]o:")
+      or string.match(newurl, "^vine:")
+      or string.match(newurl, "^android%-app:")
+      or string.match(newurl, "^ios%-app:")
+      or string.match(newurl, "^data:")
+      or string.match(newurl, "^irc:")
+      or string.match(newurl, "^%${")
+    ) then
+      check(urlparse.absolute(url, newurl))
+    end
+  end
+
+  if string.match(url, "^https?://uploadir%.com/u/") then
+    check(urlparse.absolute(url, "/uploads/" .. string.match(url, "/u/(.*)$")))
+  end
+
+  if allowed(url) and status_code < 300
+    and not string.match(url, "^https?://uploadir%.com/u/[0-9a-zA-Z]+$")
+    and not string.match(url, "^https?://uploadir%.com/uploads/[0-9a-zA-Z]+$") then
+    html = read_file(file)
+    if string.match(url, "/downloads/new") then
+      local csrf_token = string.match(html, '<meta%s+name="csrf%-token"%s*content="([^"]+)"')
+print(urlparse.escape(csrf_token))
+      table.insert(urls, {
+        url="https://uploadir.com/uploads/" .. item_value .. "/downloads",
+        method="POST",
+        body_data=
+          "authenticity_token=" .. urlparse.escape(csrf_token)
+          .. "&upload_id=" .. item_value
+          .. "&commit=Download"
+      })
+    end
+  end
+
+  return urls
+end
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  status_code = http_stat["statcode"]
+  
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. " \n")
+  io.stdout:flush()
+
+  if killgrab then
+    return wget.actions.ABORT
+  end
+
+  find_item(url["url"])
+
+  if abortgrab then
+    abort_item()
+    return wget.actions.ABORT
+  end
+
+  if string.match(url, "^https?://uploadir%.com/uploads/[0-9a-zA-Z]+$") then
+    main_download = true
+  end
+
+  if status_code == 0 or
+    (status_code ~= 200 and status_code ~= 404 and status_code ~= 302) then
+    io.stdout:write("Server returned bad response. Sleeping.\n")
+    io.stdout:flush()
+    local maxtries = 3
+    tries = tries + 1
+    if tries > maxtries then
+      tries = 0
+      abort_item()
+      return wget.actions.ABORT
+    end
+    os.execute("sleep " .. math.random(
+      math.floor(math.pow(2, tries-0.5)),
+      math.floor(math.pow(2, tries))
+    ))
+    return wget.actions.CONTINUE
+  end
+
+  tries = 0
+
+  return wget.actions.NOTHING
+end
+
+wget.callbacks.before_exit = function(exit_status, exit_status_string)
+  if killgrab or not main_download then
+    return wget.exits.IO_FAIL
+  end
+  if abortgrab then
+    abort_item()
+    return wget.exits.IO_FAIL
+  end
+  return exit_status
+end
+
